66dee62c9bce00117bb98571f5a2007be0d6a6ea
Varun Pai <varun.pai@airbnb.com>
2016-06-07 00:08:58 -0700

    add on_a_bike page skeleton

---
+++ assets/js/on_a_bike/index.js
@@ -0,0 +1,415 @@
+const cityData = {
+  "6/17/2016": {
+   "city": "Portland, ME",
+   "miles": 0
+  },
+  "6/18/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "6/19/2016": {
+   "city": "Portland, ME",
+   "miles": 0
+  },
+  "6/20/2016": {
+   "city": "Kittery, ME",
+   "miles": 5
+  },
+  "6/21/2016": {
+   "city": "Andover, MA",
+   "miles": 2
+  },
+  "6/22/2016": {
+   "city": "Fitchburg, MA",
+   "miles": 1
+  },
+  "6/23/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "6/24/2016": {
+   "city": "Northampton, MA",
+   "miles": 0
+  },
+  "6/25/2016": {
+   "city": "Pittsfield, MA",
+   "miles": 1
+  },
+  "6/26/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "6/27/2016": {
+   "city": "Poughkeepsie, NY",
+   "miles": 2
+  },
+  "6/28/2016": {
+   "city": "Port Jervis, NY",
+   "miles": 9
+  },
+  "6/29/2016": {
+   "city": "Mount Pocono, PA",
+   "miles": 5
+  },
+  "6/30/2016": {
+   "city": "Berwick, PA",
+   "miles": 5
+  },
+  "7/1/2016": {
+   "city": "State College, PA",
+   "miles": 0
+  },
+  "7/2/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "7/3/2016": {
+   "city": "Johnstown, PA",
+   "miles": 5
+  },
+  "7/4/2016": {
+   "city": "Pittsburgh, PA",
+   "miles": 9
+  },
+  "7/5/2016": {
+   "city": "Day Off",
+   "miles": 0
+  },
+  "7/6/2016": {
+   "city": "Cadiz, OH",
+   "miles": 7
+  },
+  "7/7/2016": {
+   "city": "Coshocton, OH",
+   "miles": 7
+  },
+  "7/8/2016": {
+   "city": "Columbus, IN",
+   "miles": 7
+  },
+  "7/9/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "7/0/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "7/1/2016": {
+   "city": "Dayton, OH",
+   "miles": 5
+  },
+  "7/2/2016": {
+   "city": "Rushville, IN",
+   "miles": 2
+  },
+  "7/3/2016": {
+   "city": "Bloomington, IL",
+   "miles": 6
+  },
+  "7/4/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "7/5/2016": {
+   "city": "Lawrenceville, IL",
+   "miles": 5
+  },
+  "7/6/2016": {
+   "city": "Salem, IL",
+   "miles": 1
+  },
+  "7/7/2016": {
+   "city": "St. Louis, MO",
+   "miles": 2
+  },
+  "7/8/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "7/9/2016": {
+   "city": "Bluffton, MO",
+   "miles": 4
+  },
+  "7/0/2016": {
+   "city": "Osage Lake, MO",
+   "miles": 2
+  },
+  "7/1/2016": {
+   "city": "Springfield, MO",
+   "miles": 4
+  },
+  "7/2/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "7/3/2016": {
+   "city": "Joplin, MO",
+   "miles": 0
+  },
+  "7/4/2016": {
+   "city": "Vinita, OK",
+   "miles": 9
+  },
+  "7/5/2016": {
+   "city": "Tulsa, OK",
+   "miles": 9
+  },
+  "7/6/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "7/7/2016": {
+   "city": "Chandler, OK",
+   "miles": 9
+  },
+  "7/8/2016": {
+   "city": "Yukon, OK",
+   "miles": 4
+  },
+  "7/9/2016": {
+   "city": "Foss, TX",
+   "miles": 9
+  },
+  "7/0/2016": {
+   "city": "Shamrock, TX",
+   "miles": 8
+  },
+  "7/1/2016": {
+   "city": "Amarillo, TX",
+   "miles": 7
+  },
+  "8/1/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "8/2/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "8/3/2016": {
+   "city": "Friona, TX",
+   "miles": 1
+  },
+  "8/4/2016": {
+   "city": "Portales, NM",
+   "miles": 6
+  },
+  "8/5/2016": {
+   "city": "Roswell, NM",
+   "miles": 9
+  },
+  "8/6/2016": {
+   "city": "Day Off",
+   "miles": 0
+  },
+  "8/7/2016": {
+   "city": "Carrizozo, NM",
+   "miles": 9
+  },
+  "8/8/2016": {
+   "city": "Socorro, NM",
+   "miles": 6
+  },
+  "8/9/2016": {
+   "city": "Pie Town, NM",
+   "miles": 4
+  },
+  "8/0/2016": {
+   "city": "Springerville, AZ",
+   "miles": 1
+  },
+  "8/1/2016": {
+   "city": "Heber-Overgaard, AZ",
+   "miles": 6
+  },
+  "8/2/2016": {
+   "city": "Payson, AZ",
+   "miles": 4
+  },
+  "8/3/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "8/4/2016": {
+   "city": "Cottonwood, AZ",
+   "miles": 2
+  },
+  "8/5/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "8/6/2016": {
+   "city": "Flagstaff, AZ",
+   "miles": 0
+  },
+  "8/7/2016": {
+   "city": "Grand Canyon South Rim, AZ",
+   "miles": 8
+  },
+  "8/8/2016": {
+   "city": "Day Off",
+   "miles": 0
+  },
+  "8/9/2016": {
+   "city": "Williams, AZ",
+   "miles": 9
+  },
+  "8/0/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "8/1/2016": {
+   "city": "Seligman, AZ",
+   "miles": 2
+  },
+  "8/2/2016": {
+   "city": "Kingman, AZ",
+   "miles": 7
+  },
+  "8/3/2016": {
+   "city": "Needles, AZ",
+   "miles": 3
+  },
+  "8/4/2016": {
+   "city": "Bagdad, CA",
+   "miles": 0
+  },
+  "8/5/2016": {
+   "city": "Calico, CA",
+   "miles": 0
+  },
+  "8/6/2016": {
+   "city": "Victorville, CA",
+   "miles": 1
+  },
+  "8/7/2016": {
+   "city": "Wrightwood, CA",
+   "miles": 2
+  },
+  "8/8/2016": {
+   "city": "Palmdale, CA",
+   "miles": 7
+  },
+  "8/9/2016": {
+   "city": "Build Day",
+   "miles": 0
+  },
+  "8/0/2016": {
+   "city": "Santa Clarita, CA",
+   "miles": 0
+  },
+  "8/1/2016": {
+   "city": "Santa Paula, CA",
+   "miles": 5
+  },
+  "9/1/2016": {
+   "city": "Santa Barbara, CA",
+   "miles": 0
+  },
+  "9/2/2016": {
+   "city": "Santa Barbara, CA",
+   "miles": 0
+  }
+}
+
+function App({ cityData }) {
+  return (
+    <div>
+      <Header {...{ cityData }} />
+      <GoogleMap />
+      <RecentPhotos />
+      <RecentTweets />
+    </div>
+  );
+}
+
+function Header({ cityData }) {
+  const todaysDate = moment().add(20, 'days').format('M/D/YYYY');
+  const dates = Object.keys(cityData);
+  const dayNumber = dates.indexOf(todaysDate);
+
+  const milesCompleted = dates.slice(dayNumber).map((date) => (
+    cityData[date].miles
+  )).reduce((a, b) => (
+    a + b
+  ));
+
+  const currentCity = cityData[todaysDate].city;
+
+  return (
+    <div>
+      <p>Today is <span>{todaysDate}</span></p>
+      <p>Varun is currently in <span>{currentCity}</span></p>
+      <p>He's completed <span>{milesCompleted}</span> miles so far</p>
+    </div>
+  );
+}
+
+class GoogleMap extends React.Component {
+  componentDidMount() {
+    const mapElement = ReactDOM.findDOMNode(this.refs.map);
+    const map = new google.maps.Map(mapElement, {
+      center: {lat: 39.5, lng: -98.35},
+      zoom: 3,
+    });
+
+    // Directions renderer.
+    const rendererOptions = { map };
+    const directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions)
+    directionsDisplay.setOptions({ preserveViewport: true });
+
+    // Directions request.
+    var request = {
+      origin: 'Portland, Maine',
+      destination: 'Chicago',
+      travelMode: google.maps.TravelMode.WALKING
+    };
+
+    // Set up DirectionsService.
+    const directionsService = new google.maps.DirectionsService();
+
+    directionsService.route(request, function(response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div
+        className="gmap"
+        ref="map"
+      >
+        {/* Fill in with Google Map. */}
+      </div>
+    );
+  }
+}
+
+function RecentPhotos({ cityData }) {
+  return (
+    <div>
+      map
+      {/* Fill in with recent photos. */}
+    </div>
+  );
+}
+
+function RecentTweets({ cityData }) {
+  return (
+    <div>
+      map
+      {/* Fill in with recent photos. */}
+    </div>
+  );
+}
+
+
+ReactDOM.render(
+  <App {...{ cityData }} />,
+  document.getElementById('app-container')
+);
+
+
