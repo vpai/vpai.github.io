da6e35060c4f77da21539e32da5f2804b6651890
Varun Pai <vpai@users.noreply.github.com>
2016-07-07 17:03:45 -0400

    trial and error not working

--- assets/js/on_a_bike/index.js
+++ assets/js/on_a_bike/index.js
@@ -502,7 +502,7 @@ class RecentPhotos extends React.Component {
     }).always(() => {
       $('.grid').masonry({
         itemSelector: '.grid-item',
-        columnWidth: 245,
+        columnWidth: 255,
       });
     });
   }
