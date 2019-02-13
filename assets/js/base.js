b2e96e3799a8dcac49ce1945ac79609cf2b4617b
Varun Pai <varun@Varuns-iMac.local>
2019-02-13 17:06:38 -0500

    add contact page, add fade in animations on load

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,6 +1,10 @@
+// Make menu sticky for blog posts.
 var header = document.querySelector('header');
 var postHeader = document.querySelector('.post--title');
-var origOffsetY = postHeader.offsetTop + postHeader.offsetHeight;
+
+if (postHeader) {
+  var origOffsetY = postHeader.offsetTop + postHeader.offsetHeight;
+}
 
 function onScroll(e) {
   window.scrollY > origOffsetY ? header.classList.add('sticky') :
@@ -8,3 +12,13 @@ function onScroll(e) {
 }
 
 document.addEventListener('scroll', onScroll);
+
+// Don't animate menu if the referrer is the current site.
+if (document.referrer.includes("localhost:4000")) {
+  var menuItems = document.querySelectorAll('.fade-in')
+
+  menuItems.forEach(function(menuItem) {
+    menuItem.style.webkitAnimationPlayState = "paused";
+    menuItem.style.opacity = 1;
+  });
+}
